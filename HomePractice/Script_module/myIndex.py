bfed6445b11a9ee215dbb7de304b0854c7d26085
Johnson <jschou.mail@gmail.com>
2025-11-10 20:10:29 +0800

    rev factorial 2

--- HomePractice/Script_module/myIndex.py
+++ HomePractice/Script_module/myIndex.py
@@ -1,7 +1,7 @@
-from myModule.caculate import add,sub,compute
+from myModule.caculate import add,sub,compute,compute1
 
 print("5+2=",add(5,2))
 print("5-2=",sub(5,2))
-#print("5!=",compute(5))
 
+print("5!=",compute1(5))
 print(compute([5,7,11]))

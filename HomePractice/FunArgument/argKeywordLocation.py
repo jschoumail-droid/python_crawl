f14117dc303c6047caf71460611561a5443409ea
Johnson <jschou.mail@gmail.com>
2025-11-11 20:01:25 +0800

    rev file name

--- HomePractice/FunArgument/argKeywordLocation.py
+++ HomePractice/FunArgument/argKeywordLocation.py
@@ -8,6 +8,16 @@ if __name__ =='__main__':
 def convertUsdToAud(amount,rate=0.75):
     return amount/rate
 
+def convertCurrency(amount,rate,margin=0):
+    return amount*rate*(1+margin)
+
+def computeUsdTotal(amountInAud=0,amountIngbp=0):
+    total=0
+    total+=convertCurrency(amountInAud,0.78)
+    total+=convertCurrency(amountIngbp,1.29,0.01)
+    return total
+
 if __name__ =='__main__':
     print(convertUsdToAud(100))
     print(convertUsdToAud(100,0.78))
+    print(computeUsdTotal(amountIngbp=10))

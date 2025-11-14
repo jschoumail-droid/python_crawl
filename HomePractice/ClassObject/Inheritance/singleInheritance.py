7b9b5a78644aa1eb67a6e42b15fce45a32b9e0a6
Johnson <jschou.mail@gmail.com>
2025-11-14 13:58:52 +0800

    rev classobj 2

--- HomePractice/ClassObject/Inheritance/singleInheritance.py
+++ HomePractice/ClassObject/Inheritance/singleInheritance.py
@@ -1,20 +1,49 @@
-class Animal():
-    def __init__(self,name):
-        self.name=name #private attribute，可由arg設定
-    def fly(self): #public method
-        print(self.name+"很會飛")
+class Australian():
+    isHuman=True #public attribute
+    enjoySport=True #public attribute
 
-class Bird(Animal):
-    def __init__(self,name):
-        self.name="粉紅色"+name #覆蓋富類別屬性，private attribute，可由arg設定
-    def sing(self): #public method
-        print(self.name+"也會唱歌")
+#john=Australian #不正確指定，但未顯示錯誤，下面statement除了type()有異外，其它皆無異樣。
+john=Australian()
+print(type(john))
+print("iohn is human:",john.isHuman)
+#john.enjoySport=False #correct
+print("enjoy sport:",john.enjoySport)
 
-pigeon=Animal("小白鴿")
-#pigeon.name="??" #correct
-pigeon.fly() #public method
+#ex add docstring
+'''class Pet():
+    """
+    a class to capture userfull information regarding my pets, ...
+    """
+    isHuman=False
+    owner="Michael Smith"
 
-parrot=Bird("小鸚鵡")
-#parrot.name="xxx" #correct
-parrot.fly() #inheried method
-parrot.sing() 
+chubbles=Pet()
+print("chubble is human:",chubbles.isHuman)
+print("owner :",chubbles.owner)
+print(chubbles.__doc__)'''
+
+#ex __init__方法，當用類別初始化一個物件時，它會被呼叫。
+class Pet():
+    """
+    a class to capture userfull information regarding my pets, ...
+    """
+    def __init__(self,height):
+        self.height=height
+    
+    isHuman=False
+    owner="Michael Smith"
+
+chubbles=Pet(5)
+#chubbles=Pet(height=5) #correct, use key args
+print("chubble height:",chubbles.height)
+
+#ex key args with default value
+class Country():
+    def __init__(self,name="unspecified",population=None,sizeKmsq=None):
+        self.name=name
+        self.population=population
+        self.sizeKmsq=sizeKmsq
+
+usa=Country(name='United State of America',sizeKmsq=9.8e6)
+#查看物件的特性清單
+print("dict: ",usa.__dict__)

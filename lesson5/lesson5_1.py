9cc622c31017128a4afcfcf9046b72157b236d50
ChouJohnson <jschou.mail@gmail.com>
2025-11-29 10:15:33 +0800

    add lesswon5_1

---
+++ lesson5/lesson5_1.py
@@ -0,0 +1,34 @@
+from playwright.sync_api import sync_playwright 
+import os
+
+def get_html_path()->str:
+    path=os.path.dirname(os.path.abspath(__file__))
+    html_path=os.path.join(path,"waiting_demo.html")
+
+    return f'file://{html_path}'
+
+
+def main():
+    html_path=get_html_path()
+    print(html_path)
+
+    with sync_playwright() as p:
+        # Launch the browser
+        browser=p.chromium.launch(headless=False,slow_mo=500)
+        # Create a new page
+        page=browser.new_page()
+        # Navigate to the HTML file
+        page.goto(html_path)
+        # Wait for the page to load
+        page.wait_for_load_state("networkidle")
+
+
+        # Wait for 3 seconds
+        page.wait_for_timeout(3000)
+
+        browser.close()
+
+    print(html_path)
+
+if __name__=="__main__":
+    main()

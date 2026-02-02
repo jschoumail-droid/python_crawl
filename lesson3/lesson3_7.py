b1d6724261203466c66134ecbfdbd86825ec12c7
jschoumail-droid <jschou.mail@gmail.com>
2026-02-03 00:42:22 +0800

    rev

--- lesson3/lesson3_7.py
+++ lesson3/lesson3_7.py
@@ -17,3 +17,40 @@ def run(playwright: Playwright):
 
 with sync_playwright() as playwright:
     run(playwright)
+
+'''
+# 此例每有使用 with 語法，就必須手動管理啟動與關閉
+
+from playwright.sync_api import sync_playwright
+
+# 1. 呼叫函式取得管理器
+manager = sync_playwright() 
+
+# 2. 手動啟動（這就是 with 幫你做的事）
+p = manager.start() 
+
+# 3. 此時 p 是物件，可以操作
+browser = p.chromium.launch(headless=False)
+
+# 4. 結束後必須手動關閉
+p.stop()
+'''
+
+
+'''
+# Terminates this instance of Playwright in case
+# it was created bypassing the Python context manager.
+# This is useful in REPL applications.
+
+from playwright.sync_api import sync_playwright
+
+playwright = sync_playwright().start()
+
+browser = playwright.chromium.launch()
+page = browser.new_page()
+page.goto("https://playwright.dev/")
+page.screenshot(path="example.png")
+browser.close()
+
+playwright.stop()
+'''
